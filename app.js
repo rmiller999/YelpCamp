f4d5562920b88dd5202c1e7e45de407c77379187
Reid Miller <reidmiller999@gmail.com>
2020-02-20 15:12:15 -0800

    Added models directory

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require("express"),
       app = express(),
       bodyParser = require("body-parser"),
-      mongoose = require("mongoose");
+      mongoose = require("mongoose"),
+      Campground = require("./models/campground");
+      // Campground = require("./models/campground"),
+      // Campground = require("./models/campground");
+
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// SCHEMA SETUP
-const campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-const Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 //   {
